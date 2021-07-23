5a1d0b8675d5a9e4b3d2766c2a4e7d5d41403ad0
Arlord5463 <87767614+Arlord5463@users.noreply.github.com>
2021-07-23 16:35:06 +0300

    s

--- Run Circle.py
+++ Run Circle.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 WIDTH = 400
 HEIGHT = 400
 FPS = 60
@@ -22,6 +21,8 @@ screen.fill(WHITE)
 pygame.draw.circle(screen, ORANGE, (x, y), r)
 pygame.display.update()
 
+motion = 'Stop'
+
 pygame.init()
 running = True
 while running:
@@ -29,21 +30,31 @@ while running:
     for i in events:
         if i.type == pygame.QUIT:
             running = False
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                motion = 'Left'
+            elif i.key == pygame.K_RIGHT:
+                motion = 'Right'
+            elif i.key == pygame.K_DOWN:
+                motion = 'Down'
+            elif i.key == pygame.K_UP:
+                motion = 'Up'
+        if i.type == pygame.KEYUP:
+            if i.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP]:
+                motion = 'Stop'
+    if motion == 'Left':
+        x -= 2
+    elif motion == 'Right':
+        x += 2
+    elif motion == 'Down':
+        y += 2
+    elif motion == 'Up':
+        y -= 2
 
     screen.fill(WHITE)
     pygame.draw.circle(screen, ORANGE, (x, y), r)
     pygame.display.update()
     keys = pygame.key.get_pressed()
-    moiti
-    if x >= WIDTH + r:
-        x = -19
-    elif x <= 0 - r:
-        x = 419
-    else:
-        if keys[pygame.K_d]:
-            x += 2
-        elif keys[pygame.K_a]:
-            x -= 2
 
     clock.tick(FPS)
 pygame.quit()
